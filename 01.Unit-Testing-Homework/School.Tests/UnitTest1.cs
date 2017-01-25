d9d4fbfaa5a59cf541c793f4d8119242cf3e6075
Encho Enevski <encho.enevski@gmail.com>
2017-01-26 00:45:58 +0200

    Add first homework

---
+++ 01.Unit-Testing-Homework/School.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace School.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ 01.Unit-Testing-Homework/School/School.cs
@@ -0,0 +1,9 @@
+﻿namespace School
+{
+    using System;
+
+    public class School
+    {
+
+    }
+}
